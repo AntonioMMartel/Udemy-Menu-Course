5a91cfc214f113223123ea2f452b857a0b3dec63
Antonio ML <AntonioMMartel@gmail.com>
2023-09-07 17:57:57 +0100

    Added resolution changes

--- Assets/Scripts/GraphicsMenu.cs
+++ Assets/Scripts/GraphicsMenu.cs
@@ -11,7 +11,11 @@ public class GraphicsMenu : MonoBehaviour
     public GameObject graphicsMenu;
     public Toggle fullscreenToggle, vsyncToggle;
 
+    public ResolutionItem[] resolutions;
 
+    private int selectedResolution = 0;
+
+    public Text resolutionLabel;
 
     // Start is called before the first frame update
     void Start()
@@ -25,8 +29,33 @@ public class GraphicsMenu : MonoBehaviour
         
     }
 
+    public void nextResolution() {
+        if(selectedResolution >= resolutions.Length - 1){
+            selectedResolution = 0;
+        } else {
+            selectedResolution++;
+        }
+        updateResolutionLabel();
+    }
+
+
+
+    public void previousResolution() {
+        if(selectedResolution <= 0){
+            selectedResolution = resolutions.Length - 1;
+        } else {
+            selectedResolution--;
+        }
+        updateResolutionLabel();
+    }
+
+    private void updateResolutionLabel() {
+        resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + " x " + resolutions[selectedResolution].vertical.ToString();
+    }
+
+
     public void applyChanges() {
-        Screen.fullScreen = fullscreenToggle.isOn;
+        //Screen.fullScreen = fullscreenToggle.isOn;
 
         if(vsyncToggle.isOn){
             QualitySettings.vSyncCount = 1;
@@ -34,10 +63,16 @@ public class GraphicsMenu : MonoBehaviour
             QualitySettings.vSyncCount = 0;
         }
         
+        // setResolution
+        Screen.SetResolution(resolutions[selectedResolution].horizontal, resolutions[selectedResolution].vertical, fullscreenToggle.isOn);
+
     }
 
     public void CloseMenu() {
         Destroy(graphicsMenu);
     }
 
+    [System.Serializable] public class ResolutionItem {
+        public int horizontal, vertical;
+    }
 }
