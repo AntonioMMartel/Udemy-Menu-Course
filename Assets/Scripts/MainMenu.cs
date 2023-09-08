757e4854cc78d5e2cb5ab37f30a036cd240bdc28
Antonio ML <AntonioMMartel@gmail.com>
2023-09-08 12:24:28 +0100

    Now sound is saved when game is closed

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -12,15 +12,13 @@ public class MainMenu : MonoBehaviour
     public GameObject optionsMenu;
     public GameObject canvas;
 
-    public AudioMixer audioMixer;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        audioMixer.SetFloat("MasterVolume", PlayerPrefs.GetFloat("MasterVolume"));
-        audioMixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));
-        audioMixer.SetFloat("SFXVolume", PlayerPrefs.GetFloat("SFXVolume"));
+
     }
 
     // Update is called once per frame
