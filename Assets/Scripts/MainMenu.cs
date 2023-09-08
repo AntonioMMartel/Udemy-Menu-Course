70700b6b3d2ac2820a2af3de0050861cf33a7d20
Antonio ML <AntonioMMartel@gmail.com>
2023-09-08 12:23:32 +0100

    Made sound menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
@@ -11,11 +12,15 @@ public class MainMenu : MonoBehaviour
     public GameObject optionsMenu;
     public GameObject canvas;
 
+    public AudioMixer audioMixer;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioMixer.SetFloat("MasterVolume", PlayerPrefs.GetFloat("MasterVolume"));
+        audioMixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));
+        audioMixer.SetFloat("SFXVolume", PlayerPrefs.GetFloat("SFXVolume"));
     }
 
     // Update is called once per frame
--- Assets/Scripts/SoundsMenu.cs
+++ Assets/Scripts/SoundsMenu.cs
@@ -14,10 +14,27 @@ public class SoundsMenu : MonoBehaviour
 
     public Text masterVolume, musicVolume, sfxVolume;
 
+    public AudioSource sfxLoop;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(PlayerPrefs.HasKey("MasterVolume")){
+            masterSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+            audioMixer.SetFloat("MasterVolume", PlayerPrefs.GetFloat("MasterVolume"));   
+        }
+
+        if(PlayerPrefs.HasKey("MusicVolume")){
+            musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+            audioMixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));
+        }
+
+        if(PlayerPrefs.HasKey("SFXVolume")){
+            sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+            audioMixer.SetFloat("SFXVolume", PlayerPrefs.GetFloat("SFXVolume"));
+        }
+        sfxVolume.text = (sfxSlider.value + 80).ToString();
+        masterVolume.text = (masterSlider.value + 80).ToString();
+        musicVolume.text = (musicSlider.value + 80).ToString();
     }
 
     // Update is called once per frame
@@ -35,17 +52,31 @@ public class SoundsMenu : MonoBehaviour
         masterVolume.text = (masterSlider.value + 80).ToString();
 
         audioMixer.SetFloat("MasterVolume", masterSlider.value);
+
+        PlayerPrefs.SetFloat("MasterVolume", masterSlider.value);
     }
 
     public void SetMusicVolume() {
         musicVolume.text = (musicSlider.value + 80).ToString();
         
         audioMixer.SetFloat("MusicVolume", musicSlider.value);
+
+        PlayerPrefs.SetFloat("MusicVolume", musicSlider.value);
     }
 
     public void SetSfxVolume() {
         sfxVolume.text = (sfxSlider.value + 80).ToString();
         
         audioMixer.SetFloat("SFXVolume", sfxSlider.value);
+
+        PlayerPrefs.SetFloat("SFXVolume", sfxSlider.value);
+    }
+
+    public void PlaySfxLoop() {
+        sfxLoop.Play();
+    }
+
+    public void StopSfxLoop() {
+        sfxLoop.Stop();
     }
 }
